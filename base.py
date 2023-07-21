3daa4017baf753b2a316d10264f542474fe33231
zota <zota@example.com>
2023-07-22 07:02:43 +0900

    temp

--- base.py
+++ base.py
@@ -15,19 +15,28 @@ def rotZ(th):
         [          0,          0, 1.0]
         ])
 
-v1 = np.array([10,0,1.0])
-v2 = np.array([0,10,1.0])
+def tr(x,y):
+    return np.array([
+        [        1.0,          0,   x],
+        [          0,        1.0,   y],
+        [          0,          0, 1.0]
+        ])
 
-vd1 = np.dot(v1,rotZ(0.2))
-vd2 = np.dot(v2,rotZ(0.2))
+
+v1 = np.array([[10,0,1.0],[0,10,1.0],[10,10,1.0]]).T
+
+th = 0.3
+vd1 = np.dot(v1[:,0],rotZ(th))
+vd2 = np.dot(v1[:,1],rotZ(th))
+vd3 = np.dot(v1[:,2],rotZ(th))
 
 
 fig,ax = plt.subplots(figsize = (5, 5))
 
 ax.scatter(v1[0],v1[1])
-ax.scatter(v2[0],v2[1])
 ax.scatter(vd1[0],vd1[1])
 ax.scatter(vd2[0],vd2[1])
+ax.scatter(vd3[0],vd3[1])
 
 # 軸ラベルの設定
 ax.set_xlabel("x", fontsize = 16)
