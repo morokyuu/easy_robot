9950d9d574d8acd32537e0a058e933212d3574f5
zota <zota@example.com>
2023-07-23 07:30:43 +0900

    link graphic

--- base.py
+++ base.py
@@ -22,34 +22,49 @@ def tr(x,y):
         [          0,          0, 1.0]
         ])
 
-
-l1 = 12
-l2 = 8
-l3 = 5
-
-th1 = np.linspace(0, 1.57, 10)
-th2 = 0.9
-R01 = np.dot(rotZ(th1),tr(l1,0))
-R12 = np.dot(rotZ(th2),tr(l2,0))
-
-p0 = np.array([0,0,1])
-p1 = np.dot(R01,p0)
-p2 = np.dot(np.dot(R01,R12),p0)
+def drawLine(ax, xy0, xy1, color="green"):
+    v = np.vstack((xy0[:2],xy1[:2])).T
+    ax.plot(v[0],v[1], color=color)
 
 
-fig,ax = plt.subplots(figsize = (5, 5))
-ax.scatter(0,0)
-ax.scatter(p1[0],p1[1])
-ax.scatter(p2[0],p2[1])
 
-# 軸ラベルの設定
-ax.set_xlabel("x", fontsize = 16)
-ax.set_ylabel("y", fontsize = 16)
 
-# 軸範囲の設定
-ax.set_xlim(-20, 20)
-ax.set_ylim(-20, 20)
+def robot(lp,th):
+    R01 = np.dot(rotZ(th[0]),tr(lp[0,0],lp[0,1]))
+    R12 = np.dot(rotZ(th[1]),tr(lp[1,0],lp[1,1]))
+    R23 = np.dot(rotZ(th[2]),tr(lp[2,0],lp[2,1]))
+    
+    p0 = np.array([0,0,1])
+    p1 = np.dot(R01,p0)
+    p2 = np.dot(np.dot(R01,R12),p0)
+    p3 = np.dot(np.dot(np.dot(R01,R12),R23),p0)
+    
+    fig,ax = plt.subplots(figsize = (5, 5))
+    ax.scatter(0,0)
+    ax.scatter(p1[0],p1[1])
+    ax.scatter(p2[0],p2[1])
+    ax.scatter(p3[0],p3[1])
+    # link
+    drawLine(ax, p0, p1)
+    drawLine(ax, p1, p2)
+    drawLine(ax, p2, p3)
+    # 軸ラベルの設定
+    ax.set_xlabel("x", fontsize = 16)
+    ax.set_ylabel("y", fontsize = 16)
+    # 軸範囲の設定
+    ax.set_xlim(-30, 30)
+    ax.set_ylim(-30, 30)
+    ax.grid()
 
-ax.grid()
+
+lp = np.array([
+    [12,0,1.0],
+    [ 8,0,1.0],
+    [ 5,0,1.0]
+    ])
+th = np.array([0.3,0.1,0.0])
+
+robot(lp,th)
+
 
 
